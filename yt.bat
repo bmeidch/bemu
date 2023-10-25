eb84df090134924d5ac8b9245c1faaa10ed097bb
bmeidch <86718947+bmeidch@users.noreply.github.com>
2023-10-25 21:58:03 +0700

    Update yt.bat

--- yt.bat
+++ yt.bat
@@ -5,6 +5,7 @@ python tvone.py > ../lokal/tvone.m3u8
 python r21.py > ../lokal/r21.m3u8
 python nettv.py > ../lokal/nettv.m3u8
 python metrotv.py > ../lokal/metrotv.m3u8
+python metrotvlr.py > ../lokal/metrotvlr.m3u8
 python kompastv.py > ../lokal/kompastv.m3u8
 python kompastvlr.py > ../lokal/kompastvlr.m3u8
 python hope.py > ../lokal/hope.m3u8
