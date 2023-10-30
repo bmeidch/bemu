eb8a623403801fda6a86139059e17a7cd521f73a
bmeidch <86718947+bmeidch@users.noreply.github.com>
2023-10-30 11:57:04 +0700

    Update index.py

--- epg/scripts/index.py
+++ epg/scripts/index.py
@@ -29,7 +29,7 @@ def merge(tree, tagname, attrib):
                             if child.attrib[attrib] == rmeof[0]:
                                 child.attrib[attrib] = rmeof[1] 
                             if child.attrib[attrib] == rmeof[1]:
-                                child.attrib[attrib] = rmeof[1] 
+                                child.attrib[attrib] = rmeof[0] 
                     tree.append(child)
         except:
             print('Skipping:', file)
