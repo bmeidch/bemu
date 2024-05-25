02ef071e6fb61680f38ff47cba325a26ae3ba70b
bmeidch <86718947+bmeidch@users.noreply.github.com>
2024-05-25 12:29:27 +0700

    Add files via upload

---
+++ epg/scripts/epg-id.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /home/runner/work/bemu/bemu/iptv-org-epg && npm install
+
+# IDHM EPG
+
+npm run grab -- --site=indihometv.com --output=../epg/id/epg-idhm.xml --days=7 --maxConnections=2
+
+# VIS EPG
+
+npm run grab -- --site=visionplus.id --output=../epg/id/epg-visionplus.xml --days=7 --maxConnections=10
+
+# VID EPG
+
+npm run grab -- --site=vidio.com --output=../epg/id/epg-vidio-com.xml --days=7 --maxConnections=5
+
+# CM EPG
+
+npm run grab -- --site=cubmu.com --output=../epg/id/epg-cubmu.xml --days=7 --maxConnections=5
+
+# DS EPG
+
+npm run grab -- --site=dens.tv --output=../epg/id/epg-dens.xml --days=7 --maxConnections=5
+
+# Compress EPG xml files
+
+xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
+
+# Remove EPG xml files
+
+rm epg*.xml
+
+exit 0
