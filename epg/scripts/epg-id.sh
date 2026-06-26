dbbd64d3a3eeb005f72a34541f87a7579208413c
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-06-26 21:24:07 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -1,21 +1,107 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 cd "$GITHUB_WORKSPACE/iptv-org-epg"
 
-npm ci
+# Install dependencies
+if [[ -f package-lock.json ]]; then
+    npm ci --prefer-offline --no-audit --fund=false
+else
+    npm install --prefer-offline --no-audit --fund=false
+fi
 
-npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/aioepg.xml --days=2 --maxConnections=10
-npm run grab -- --sites=maxstream.tv --output=../epg/id-mxs.xml --days=2 --maxConnections=10
-npm run grab -- --sites=visionplus.id --output=../epg/id-vplus.xml --days=2 --maxConnections=10
-npm run grab -- --sites=vidio.com --output=../epg/id-vd.xml --days=2 --maxConnections=10
-npm run grab -- --sites=cubmu.com --output=../epg/id-cm.xml --days=2 --maxConnections=10
-npm run grab -- --sites=dens.tv --output=../epg/id-ds.xml --days=2 --maxConnections=10
-npm run grab -- --channels=../epg/scripts/mvs.xml --output=../epg/id-mvsepg.xml --days=2 --maxConnections=10
+OUTPUT="../epg"
 
-cd ../epg
+mkdir -p "$OUTPUT"
 
-gzip -kf9 *.xml
-rm -f *.xml
+compress() {
+    local file="$1"
 
-exit 0
+    gzip -kf9 "$file"
+    rm -f "$file"
+}
+
+case "${1:-}" in
+
+aio)
+    npm run grab -- \
+        --channels="$OUTPUT/scripts/aiochannels.xml" \
+        --output="$OUTPUT/aioepg.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/aioepg.xml"
+    ;;
+
+maxstream)
+    npm run grab -- \
+        --sites=maxstream.tv \
+        --output="$OUTPUT/id-mxs.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-mxs.xml"
+    ;;
+
+visionplus)
+    npm run grab -- \
+        --sites=visionplus.id \
+        --output="$OUTPUT/id-vplus.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-vplus.xml"
+    ;;
+
+vidio)
+    npm run grab -- \
+        --sites=vidio.com \
+        --output="$OUTPUT/id-vd.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-vd.xml"
+    ;;
+
+cubmu)
+    npm run grab -- \
+        --sites=cubmu.com \
+        --output="$OUTPUT/id-cm.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-cm.xml"
+    ;;
+
+dens)
+    npm run grab -- \
+        --sites=dens.tv \
+        --output="$OUTPUT/id-ds.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-ds.xml"
+    ;;
+
+mncvision)
+    npm run grab -- \
+        --channels="$OUTPUT/scripts/mvs.xml" \
+        --output="$OUTPUT/id-mvsepg.xml" \
+        --days=2 \
+        --maxConnections=10
+
+    compress "$OUTPUT/id-mvsepg.xml"
+    ;;
+
+*)
+    echo "Usage:"
+    echo "  $0 aio"
+    echo "  $0 maxstream"
+    echo "  $0 visionplus"
+    echo "  $0 vidio"
+    echo "  $0 cubmu"
+    echo "  $0 dens"
+    echo "  $0 mncvision"
+    exit 1
+    ;;
+esac
