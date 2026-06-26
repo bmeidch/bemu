75433de3c216b544916719d91fc839d5b5601df4
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-06-26 17:09:04 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -8,27 +8,27 @@ npm run grab --- --channels=../epg/scripts/aiochannels.xml --output=../epg/aioep
 
 # IDHM EPG
 
-npm run grab --- --site=maxstream.tv --output=../epg/id-mxs-id.xml --days=2 --maxConnections=10
+npm run grab --- --sites=maxstream.tv --output=../epg/id-mxs-id.xml --days=2 --maxConnections=10
 
 # VIS EPG
 
-npm run grab --- --site=visionplus.id --output=../epg/id-vplus-id.xml --days=2 --maxConnections=10
+npm run grab --- --sites=visionplus.id --output=../epg/id-vplus-id.xml --days=2 --maxConnections=10
 
 # VID EPG
 
-npm run grab --- --site=vidio.com --output=../epg/id-vd-id.xml --days=2 --maxConnections=10
+npm run grab --- --sites=vidio.com --output=../epg/id-vd-id.xml --days=2 --maxConnections=10
 
 # CM EPG
 
-npm run grab --- --site=cubmu.com --output=../epg/id-cm-id.xml --days=2 --maxConnections=10
+npm run grab --- --sites=cubmu.com --output=../epg/id-cm-id.xml --days=2 --maxConnections=10
 
 # DS EPG
 
-npm run grab --- --site=dens.tv --output=../epg/id-ds-id.xml --days=2 --maxConnections=10
+npm run grab --- --sites=dens.tv --output=../epg/id-ds-id.xml --days=2 --maxConnections=10
 
 # MNCVISION EPG
 
-npm run grab -- --site=mncvision.id --output=../epg/id-mncvision-id.xml --days=1 --maxConnections=10
+npm run grab -- --sites=mncvision.id --output=../epg/id-mncvision-id.xml --days=1 --maxConnections=10
 
 # Compress EPG xml files
 cd ../epg/
