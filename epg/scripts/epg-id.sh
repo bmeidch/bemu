ab12fb800c8454f2630962b925d001dfd26f87c2
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-07-01 18:19:11 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /home/runner/work/M3UPT/M3UPT/iptv-org-epg && npm install
+
+cd iptv-org-epg || exit 1
+npm install
 
 npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/files/aioepg.xml --days=2 --maxConnections=10
 npm run grab -- --sites=maxstream.tv --output=../epg/files/id-mxs.xml --days=2 --maxConnections=10
@@ -8,8 +10,9 @@ npm run grab -- --sites=vidio.com --output=../epg/files/id-vd.xml --days=2 --max
 npm run grab -- --sites=cubmu.com --output=../epg/files/id-cm.xml --days=2 --maxConnections=10
 npm run grab -- --sites=dens.tv --output=../epg/files/id-ds.xml --days=2 --maxConnections=10
 
-cd ../epg/files
+cd ../epg/files || exit 1
 
+# Kompresi file menjadi .xz dan .gz, lalu hapus file .xml mentah
 xz -kf9 *.xml && gzip -kf9 *.xml
 rm -f *.xml
 
