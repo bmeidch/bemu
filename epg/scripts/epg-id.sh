6cd4d646a56387e72a8be90fe2793060ba3cce42
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-07-01 23:17:31 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -3,12 +3,12 @@
 cd iptv-org-epg || exit 1
 npm ci
 
-npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/files/aioepg.xml --days=2 --maxConnections=10
-npm run grab -- --sites=maxstream.tv --output=../epg/files/id-mxs.xml --days=2 --maxConnections=10
-npm run grab -- --sites=visionplus.id --output=../epg/files/id-vplus.xml --days=2 --maxConnections=10
-npm run grab -- --sites=vidio.com --output=../epg/files/id-vd.xml --days=2 --maxConnections=10
-npm run grab -- --sites=cubmu.com --output=../epg/files/id-cm.xml --days=2 --maxConnections=10
-npm run grab -- --sites=dens.tv --output=../epg/files/id-ds.xml --days=2 --maxConnections=10
+npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/files/aioepg.xml --days=7 --maxConnections=20
+npm run grab -- --sites=maxstream.tv --output=../epg/files/id-mxs.xml --days=7 --maxConnections=20
+npm run grab -- --sites=visionplus.id --output=../epg/files/id-vplus.xml --days=7 --maxConnections=20
+npm run grab -- --sites=vidio.com --output=../epg/files/id-vd.xml --days=7 --maxConnections=20
+npm run grab -- --sites=cubmu.com --output=../epg/files/id-cm.xml --days=7 --maxConnections=20
+npm run grab -- --sites=dens.tv --output=../epg/files/id-ds.xml --days=7 --maxConnections=20
 
 cd ../epg/files || exit 1
 gzip -f9 *.xml
