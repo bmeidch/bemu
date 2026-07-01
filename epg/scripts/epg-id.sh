7e5ff4983167c342350594c40f125414385befc1
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-07-01 18:10:28 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-set -e
+cd /home/runner/work/M3UPT/M3UPT/iptv-org-epg && npm install
 
-cd "$GITHUB_WORKSPACE/iptv-org-epg"
+npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/files/aioepg.xml --days=2 --maxConnections=10
+npm run grab -- --sites=maxstream.tv --output=../epg/files/id-mxs.xml --days=2 --maxConnections=10
+npm run grab -- --sites=visionplus.id --output=../epg/files/id-vplus.xml --days=2 --maxConnections=10
+npm run grab -- --sites=vidio.com --output=../epg/files/id-vd.xml --days=2 --maxConnections=10
+npm run grab -- --sites=cubmu.com --output=../epg/files/id-cm.xml --days=2 --maxConnections=10
+npm run grab -- --sites=dens.tv --output=../epg/files/id-ds.xml --days=2 --maxConnections=10
 
-npm ci
+cd ../epg/files
 
-npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/aioepg.xml --days=2 --maxConnections=10
-npm run grab -- --sites=maxstream.tv --output=../epg/id-mxs.xml --days=2 --maxConnections=10
-npm run grab -- --sites=visionplus.id --output=../epg/id-vplus.xml --days=2 --maxConnections=10
-npm run grab -- --sites=vidio.com --output=../epg/id-vd.xml --days=2 --maxConnections=10
-npm run grab -- --sites=cubmu.com --output=../epg/id-cm.xml --days=2 --maxConnections=10
-npm run grab -- --sites=dens.tv --output=../epg/id-ds.xml --days=2 --maxConnections=10
-
-cd ../epg
-
-gzip -kf9 *.xml
+xz -kf9 *.xml && gzip -kf9 *.xml
 rm -f *.xml
 
 exit 0
