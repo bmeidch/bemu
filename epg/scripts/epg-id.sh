2e833ac42f27d37527f8f1d6136da3939801719f
bmeidch <86718947+bmeidch@users.noreply.github.com>
2026-07-02 19:00:35 +0700

    Add files via upload

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -4,7 +4,11 @@ cd iptv-org-epg || exit 1
 npm ci
 
 npm run grab -- --channels=../epg/scripts/aiochannels.xml --output=../epg/files/aioepg.xml --days=2 --maxConnections=20
-npm run grab -- --sites=maxstream.tv,visionplus.id,vidio.com,cubmu.com,dens.tv --output=../epg/files/id-{site}.xml --days=2 --maxConnections=20
+npm run grab -- --sites=maxstream.tv --output=../epg/files/id-mxs.xml --days=2 --maxConnections=20
+npm run grab -- --sites=visionplus.id --output=../epg/files/id-vplus.xml --days=2 --maxConnections=20
+npm run grab -- --sites=vidio.com --output=../epg/files/id-vd.xml --days=2 --maxConnections=20
+npm run grab -- --sites=cubmu.com --output=../epg/files/id-cm.xml --days=2 --maxConnections=20
+npm run grab -- --sites=dens.tv --output=../epg/files/id-ds.xml --days=2 --maxConnections=20
 
 cd ../epg/files || exit 1
 gzip -f9 *.xml
